fa1766a15393f37948096f8c7397ec75569afcda
[name] <sergio.gamboa11@gmail.com>
2018-12-12 16:15:46 -0700

    Added methods to print out lists, fixed bug on addFieldEquivalence class

--- MessageTemplate.py
+++ MessageTemplate.py
@@ -27,6 +27,11 @@ class MessageTemplate:
             if x.sourceFieldName == a and x.targetFieldName == b:
                 self.fieldLengthDependencyList.remove(x)
 
+    def printFieldLengthDependencyList(self):
+        for x in self.fieldLengthDependencyList:
+            print(x.sourceFieldName + " " + x.sourceFieldName)
+        print("\n")
+
     def addPacketLengthDependency(self, a, b):
         self.packetLDList.append(PacketLengthDependency(a,b))
 
@@ -34,6 +39,11 @@ class MessageTemplate:
         for x in self.packetLDList:
             if x.packetName == a and x.fieldName == b:
                 self.packetLDList.remove(x)
+
+    def printPacketLengthDependencyList(self):
+        for x in self.packetLDList:
+            print(x.packetName + " " + x.fieldName)
+        print("\n")
 
     def addCheckSum(self, a, b):
         self.checksumList.append(CheckSum(a, b))
@@ -43,17 +53,27 @@ class MessageTemplate:
             if x.packetName == a and x.fieldName == b:
                 self.checksumList.remove(x)
 
+    def printCheckSumList(self):
+        for x in self.checksumList:
+            print(x.packetName + " " + x.fieldName)
+        print("\n")
+
     def addFieldEquivalence(self, a, b, c, d):
-        self.fieldEquivalenceList.append(a,b,c,d)
+        self.fieldEquivalenceList.append(FieldEquivalence(a, b, c, d))
 
     def removeFieldEquivalence(self, a, b, c, d):
         for x in self.fieldEquivalenceList:
-            if x.SourceMessageType == a and x.sourceFieldName == b and x.targetMessageType == c and x.targetFieldName == d:
+            if x.sourceMessageType == a and x.sourceFieldName == b and x.targetMessageType == c and x.targetFieldName == d:
                 self.fieldEquivalenceList.remove(x)
 
+    def printFieldEquivalenceList(self):
+        for x in self.fieldEquivalenceList:
+            print(x.sourceMessageType + " " + x.sourceFieldName + " " + x.targetMessageType + " " + x.targetFieldName)
+        print("\n")
+
 exampleMsg = MessageTemplate("Message1","C:/Users/Sergio/Desktop", "C:/Users/Sergio/Desktop/Folder", "Scapy")
-exampleMsg.addFieldLengthDependency("a", "b")
-exampleMsg.addFieldLengthDependency("c", "d")
-print(exampleMsg.fieldLengthDependencyList[0].sourceFieldName + " " + exampleMsg.fieldLengthDependencyList[0].targetFieldName)
-exampleMsg.removeFieldLengthDependency("a", "b")
-print(exampleMsg.fieldLengthDependencyList[0].sourceFieldName + " " + exampleMsg.fieldLengthDependencyList[0].targetFieldName)
+exampleMsg.addFieldEquivalence("a", "b", "c", "d")
+exampleMsg.addFieldEquivalence("c", "d", "e", "f")
+exampleMsg.printFieldEquivalenceList()
+exampleMsg.removeFieldEquivalence("a", "b", "c", "d")
+exampleMsg.printFieldEquivalenceList()
