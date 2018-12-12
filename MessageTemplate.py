6f17e9214d0eb61c8a430a4bfac2401cba333b14
[name] <sergio.gamboa11@gmail.com>
2018-12-12 16:02:34 -0700

    added a way to remove from each list

--- MessageTemplate.py
+++ MessageTemplate.py
@@ -19,31 +19,41 @@ class MessageTemplate:
         # self.check = CheckSum("e","f")
         # self.fe = FieldEquivalence("g","h","i","j")
 
-    # def changeDestFolderName(self, name):
-    #     self.destinationFolderName = name
-    #
-    # def changeMessageTemplateName(self, name):
-    #     self.messageTemplateName = name
-    #
-    # def changeDestFolderPath(self, path):
-    #     self.destinationFolderPath = path
-    #
-    # def changeOutputFormet(self, format):
-    #     self.outputFormat = format
-
     def addFieldLengthDependency(self, a, b):
         self.fieldLengthDependencyList.append(FieldLengthDependency(a,b))
+
+    def removeFieldLengthDependency(self, a, b):
+        for x in self.fieldLengthDependencyList:
+            if x.sourceFieldName == a and x.targetFieldName == b:
+                self.fieldLengthDependencyList.remove(x)
 
     def addPacketLengthDependency(self, a, b):
         self.packetLDList.append(PacketLengthDependency(a,b))
 
+    def removePacketLengthDependency(self, a, b):
+        for x in self.packetLDList:
+            if x.packetName == a and x.fieldName == b:
+                self.packetLDList.remove(x)
+
     def addCheckSum(self, a, b):
         self.checksumList.append(CheckSum(a, b))
+
+    def removeCheckSum(self, a, b):
+        for x in self.checksumList:
+            if x.packetName == a and x.fieldName == b:
+                self.checksumList.remove(x)
 
     def addFieldEquivalence(self, a, b, c, d):
         self.fieldEquivalenceList.append(a,b,c,d)
 
+    def removeFieldEquivalence(self, a, b, c, d):
+        for x in self.fieldEquivalenceList:
+            if x.SourceMessageType == a and x.sourceFieldName == b and x.targetMessageType == c and x.targetFieldName == d:
+                self.fieldEquivalenceList.remove(x)
+
 exampleMsg = MessageTemplate("Message1","C:/Users/Sergio/Desktop", "C:/Users/Sergio/Desktop/Folder", "Scapy")
 exampleMsg.addFieldLengthDependency("a", "b")
-exampleMsg.addPacketLengthDependency("c", "d")
-print(exampleMsg.fieldLengthDependencyList[0].sourceFieldName)
+exampleMsg.addFieldLengthDependency("c", "d")
+print(exampleMsg.fieldLengthDependencyList[0].sourceFieldName + " " + exampleMsg.fieldLengthDependencyList[0].targetFieldName)
+exampleMsg.removeFieldLengthDependency("a", "b")
+print(exampleMsg.fieldLengthDependencyList[0].sourceFieldName + " " + exampleMsg.fieldLengthDependencyList[0].targetFieldName)
